1f49f1e5c8b957d87443e7011f85a503fd060f5f
nicknamejv <jonvercabral@yahoo.com>
2021-07-21 14:46:20 -0700

    Create index route with a response

--- server.js
+++ server.js
@@ -13,7 +13,29 @@ const PORT = 4000; // <--- system level configuration variables
 // SECTION: Middleware //
 
 
-// SECTION: Routes // 
+// SECTION: Routes //
+
+app.get('/', (req, res) => {
+    res.redirect('/posts');
+});
+
+
+// NOTE: Index GET / - Presentational
+app.get('/posts', (req, res) => {
+    res.send('Post index.');
+}); 
+
+// NOTE: New  GET /posts - Presentational FORM 
+
+// NOTE: Create POST /posts - Functional
+
+// NOTE: Show GET /posts/:id - Presentational
+
+// NOTE: Edit GET /posts/:id/edit - Presentational FORM
+
+// NOTE: Update PUT /posts/:id - Functional
+
+// NOTE: Destroy DELETE /posts/:id - Functional 
 
 
 // SECTION: Sever Bind //
